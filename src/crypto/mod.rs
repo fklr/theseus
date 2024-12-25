9833e18e78b6d07d3d406f21e1bb471c60f3dc21
em ▘▖▌ <146671001+mekaem@users.noreply.github.com>
2024-12-24 18:36:59 -0800

    oops

--- src/crypto/mod.rs
+++ src/crypto/mod.rs
@@ -3,6 +3,7 @@ pub mod commitment;
 pub mod merkle;
 pub mod primitives;
 pub mod proofs;
+pub mod serialize;
 pub mod signatures;
 
 pub use circuits::{Circuit, Constraint, Variable};
@@ -10,4 +11,5 @@ pub use commitment::{PedersenCommitment, StateMatrixCommitment};
 pub use merkle::SparseMerkleTree;
 pub use primitives::{CurveGroups, ProofTranscript, Scalar, G1, G2, GT};
 pub use proofs::{CircuitProof, ProofSystem};
+pub use serialize::{SerializableG1, SerializableG2, SerializableScalar};
 pub use signatures::{AggregateSignature, BlsSignature};
